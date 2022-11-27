75e6f57f3283d4f1ceef91eb96ee8953c553e743
Wahab <mailwahab247@gmail.com>
2022-11-27 07:58:26 +0000

    add login UI. close #4

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,11 +6,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
 
-  private registerUrl = "http://localhost:5050/api/v1/auth/register"
+  private registerUrl = "http://localhost:5000/api/v1/auth/register"
+  private loginUrl = "http://localhost:5000/api/v1/auth/login"
 
   constructor(private http: HttpClient) { }
 
   register(user: any) {
     return this.http.post<any>(this.registerUrl, user)
+  }
+
+  login(user: any) {
+    console.log('Should work')
+    return this.http.post<any>(this.loginUrl, user)
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(private _auth: AuthService) { }
+
+  loginUserData: any = {}
+
+  loginUser() {
+    this._auth.login(this.loginUserData).
+      subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      )
+  }
 
 }
